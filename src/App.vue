ad2a5c5a558d66ffc97f2847d166b3a8eb7de4e7
Manel Gavaldà Andreu <manelgavalda@iesebre.com>
2017-03-23 21:54:53 +0100

    Initial version

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="phone-viewport">
+    <md-toolbar>
+      <md-button class="md-icon-button" @click.native="toggleLeftSidenav">
+        <md-icon>menu</md-icon>
+      </md-button>
+
+      <h2 class="md-title">Enrollment Mobile</h2>
+    </md-toolbar>
+
+    <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+      <md-toolbar class="md-large">
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Content</h3>
+        </div>
+      </md-toolbar>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi cupiditate esse necessitatibus beatae nobis, deserunt ut est fugit, tempora deleniti, eligendi commodi doloribus. Nemo, assumenda possimus, impedit inventore perferendis iusto!</p>
+    </md-sidenav>
+
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    toggleLeftSidenav () {
+      this.$refs.leftSidenav.toggle()
+    },
+    closeRightSidenav () {
+      this.$refs.rightSidenav.close()
+    },
+    open (ref) {
+      console.log('Opened: ' + ref)
+    },
+    close (ref) {
+      console.log('Closed: ' + ref)
+    }
+  }
+}
+</script>
+<style>
+</style>
