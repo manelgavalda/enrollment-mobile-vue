d8111da9104fa63ada1752ae3fc5c5ccc687c6b0
manelgavalda <manelgavalda@iesebre.com>
2017-05-18 23:51:04 +0200

    Factory relationship ok

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const constants = {
   OAUTH_GRANT_TYPE: 'password',
   OAUTH_CLIENT_ID: 1,
-  OAUTH_CLIENT_SECRET: 'hE9hCX2GyrcTmmj987eBEkl5G0cTBtPg9uDY7Sbo',
+  OAUTH_CLIENT_SECRET: 'rUqgJcCRu9fKdezh133p0m1QyaZLJRuCCFZL8V3D',
   ANDROID_SENDER_ID: 'TODO'
 }
 export default constants
