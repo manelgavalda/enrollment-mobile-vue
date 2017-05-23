a95b49fec6a6a848f92bfcb64a5d09ddf97d54df
Manel Gavaldà Andreu <manelgavalda@iesebre.com>
2017-05-23 15:55:19 +0200

    commit

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const constants = {
   OAUTH_GRANT_TYPE: 'password',
   OAUTH_CLIENT_ID: 1,
-  OAUTH_CLIENT_SECRET: 'D9I65yppzu38JmsqbvzULhOD6xOpDLgJSDSbhWKJ',
+  OAUTH_CLIENT_SECRET: 'Sw0EK7kCLwwa56yEFl5fsFXVbVyTWOZQdpGHXiDs',
   ANDROID_SENDER_ID: 'TODO'
 }
 export default constants
