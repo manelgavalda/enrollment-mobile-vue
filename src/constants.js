8afc5a8db9c1d89dda0456856a1463b2e55a2c7c
Manel Gavaldà Andreu <manelgavalda@iesebre.com>
2017-05-23 19:28:29 +0200

    getting enrollments and info from user

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const constants = {
   OAUTH_GRANT_TYPE: 'password',
   OAUTH_CLIENT_ID: 1,
-  OAUTH_CLIENT_SECRET: 'FFehWc9ie841kHD7f2tX8qMbkMZiI34rY9YqYwDS',
+  OAUTH_CLIENT_SECRET: 'rIufZLAwmosBFhOuYIWZ7sV1vc12EXoAvP3TyRQ0',
   ANDROID_SENDER_ID: 'TODO'
 }
 export default constants
