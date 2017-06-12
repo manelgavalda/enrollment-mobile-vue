9111529c2df637aa11b820e3ab964464f767f4ed
manelgavalda <manelgavalda@iesebre.com>
2017-06-12 17:43:58 +0200

    Added table result

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const constants = {
   OAUTH_GRANT_TYPE: 'password',
   OAUTH_CLIENT_ID: 1,
-  OAUTH_CLIENT_SECRET: 'dSHLw27fcngfefWFidCVa3TL6Za4EIZyKaAFjbdO',
+  OAUTH_CLIENT_SECRET: 'CB0hD1FAf5XAOsEOijbyL5pbuy3kDt2OWGSQC1NV',
   ANDROID_SENDER_ID: 'TODO'
 }
 export default constants
