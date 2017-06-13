4285cdd61268be7c3cbb6550638a4c1726561d2d
manelgavalda <manelgavalda@iesebre.com>
2017-06-13 18:47:28 +0200

    TOken change

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const constants = {
   OAUTH_GRANT_TYPE: 'password',
   OAUTH_CLIENT_ID: 1,
-  OAUTH_CLIENT_SECRET: 'CB0hD1FAf5XAOsEOijbyL5pbuy3kDt2OWGSQC1NV',
+  OAUTH_CLIENT_SECRET: 'OVZVWORE7YjoUMq63RFWUGlzmT5alMBpS9Ot3kM8',
   ANDROID_SENDER_ID: 'TODO'
 }
 export default constants
